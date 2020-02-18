21c8ce0363db86841ab26c3e2df0203278b0ae16
Vishu <brs@Vishus-MacBook-Air.local>
2020-02-18 17:45:08 +0530

    Installed react-redux, redux, react-router, material-ui and axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </Router>
+  </Provider>,
+  rootElement
+);
